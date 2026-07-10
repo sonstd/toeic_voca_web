8cfb427f7bec68a0b2767e626dcd3ae69f467044
sonstd <thswlgh0599@gmail.com>
2026-07-10 12:13:18 +0900

    vercel connection test

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import { LEVELS } from '@/lib/words';
 import HomeClient from '@/components/HomeClient';
 
-// LEVELS는 서버에서 정적으로 읽어 클라이언트에 직렬화하여 전달
+// LEVELS는 서버에서 정적으로 읽어 클라이언트에 직렬화하여 전달함
 export default function HomePage() {
   const levels = Object.entries(LEVELS).map(([key, { label, totalDays }]) => ({
     key,
